58723c7b4ddb33ef70c57a8c16a1e1b42ccde526
Ivo Katunaric <ikatun@gmail.com>
2019-03-27 01:45:58 +0100

    Fix config for db logging

--- ormconfig/ormconfig.ts
+++ ormconfig/ormconfig.ts
@@ -4,7 +4,7 @@ export const connectionOptions: any = {
   entities: ['src/backend/data/models/**.ts', 'src/backend/data/custom-models/**.ts'],
   type: config.databaseType,
   synchronize: config.databaseSynchronize,
-  logging: false,
+  logging: config.databaseLogging,
   migrations: ['src/backend/data/migrations/**.ts'],
   cli: {
     migrationsDir: 'src/backend/data/migrations',
--- src/backend/server/config.ts
+++ src/backend/server/config.ts
@@ -3,7 +3,7 @@
 export default {
   databaseType: process.env.DATABASE_TYPE || 'sqlite',
   databaseSynchronize: !!process.env.DATABASE_SYNCHRONIZE,
-  databaseLogging: process.env.DATABASE_LOGGING!,
+  databaseLogging: process.env.DATABASE_LOGGING === 'true',
   environment: process.env.NODE_ENV!,
   port: process.env.PORT || 5001,
   serverUrl: process.env.SERVER_URL || 'http://localhost:5001',
