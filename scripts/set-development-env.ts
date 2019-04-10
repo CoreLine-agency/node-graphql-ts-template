27893fc69b82e48306382c2d8847660037a1cb59
Ivo Katunaric <ikatun@gmail.com>
2019-04-10 20:53:03 +0200

    Auto setup docker port

--- scripts/set-development-env.ts
+++ scripts/set-development-env.ts
@@ -1,9 +1,39 @@
 import { execSync } from 'child_process';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
+import { trim } from 'lodash';
+import readline from 'readline-promise';
 
-try {
-  readFileSync('.env', 'utf8');
-} catch (e) {
-  execSync('cp template.env .env');
-  console.log('Default .env file created');
-}
+const rlp = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: true,
+});
+
+// tslint:disable-next-line
+const randomPort = () => Math.floor((Math.random() * 58000) + 1100);
+
+(async () => {
+  try {
+    readFileSync('.env', 'utf8');
+  } catch (e) {
+    let templateEnv = readFileSync('template.env', 'utf8');
+    let dockerCompose = readFileSync('docker-compose.yml', 'utf8');
+    if (templateEnv.includes('technobabble-template')) {
+      const name = trim(await rlp.questionAsync('Docker database/machine/service name?'));
+      if (!name) {
+        throw new Error('Name must not be empty');
+      }
+      templateEnv = templateEnv.replace(/technobabble-template/g, name);
+
+      const port = randomPort().toString(10);
+      dockerCompose = dockerCompose.replace(/5402/g, port);
+      templateEnv = templateEnv.replace(/5402/g, port);
+
+      console.log('Setting up database for port', port);
+      writeFileSync('template.env', templateEnv, { encoding: 'utf8' });
+      writeFileSync('docker-compose.yml', dockerCompose, { encoding: 'utf8' });
+    }
+    execSync('cp template.env .env');
+    console.log('Default .env file created');
+  }
+})();
