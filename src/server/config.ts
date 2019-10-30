02f541970f769a3a00c7bff6cd5949219372f7b3
ikatun <ikatun@gmail.com>
2019-10-30 09:55:27 +0100

    Update config.ts

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,7 +1,7 @@
 // tslint:disable: no-http-string
 
 export default {
-  databaseType: process.env.DATABASE_TYPE || 'sqlite',
+  databaseType: process.env.DATABASE_TYPE || 'postgres',
   databaseSynchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
   databaseLogging: process.env.DATABASE_LOGGING === 'true',
   environment: process.env.NODE_ENV,
